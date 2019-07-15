72df090d9c22b3c93ba41094f58660c24114eef7
Aleksey Karelin <a.karelin@dlg.im>
2019-07-15 09:35:08 +0700

    Fix php-mysql install, update clean_all script

--- clean_all.sh
+++ clean_all.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-dpkg -l | grep 'mysql\|docker\|nginx'|awk '{print $2}'|xargs apt remove -y --purge && apt autoremove --purge -y && apt autoclean && rm -rf /etc/mysql /var/lib/mysql
+dpkg -l | grep 'mysql\|docker\|nginx'| \
+          awk '{print $2}'| \
+          xargs apt remove -y --purge && \
+          apt autoremove --purge -y && \
+          apt autoclean && \
+          rm -rf /var/lib/mysql* /etc/nginx /etc/mysql /var/www/*
